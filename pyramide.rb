95f348645f7ddd8dbcbbb6071e9b18781fbbe70c
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-09-28 00:44:49 +0200

    wooops , petit oublie de conditions 1 a 25 max

--- pyramide.rb
+++ pyramide.rb
@@ -1,4 +1,4 @@
-puts "Salut, bienvenue dans le générateur pyramide <<BEHIND THE MIRROR>>! Combien d'étages veux-tu ?"
+puts "Salut, bienvenue dans le générateur pyramide <<BEHIND THE MIRROR>>! Combien d'étages veux-tu ? (Attention le choix est toujours limité entre 1 et 25 étages)"
 print ">   "
 
 number = gets.to_i
@@ -6,12 +6,18 @@ number = gets.to_i
 i = 0 # edits "#" i.times
 j = number # spaces editor
 
-puts "Voici donc cette fameuse pyramide :"
+if number < 1
+	puts "une pyramide en RDC ou sous terre c'est pas glam désolé"
+elsif number > 25
+	puts "abuses pas ! on a dit 25 étages MAX !"
+else
+	puts "Voici donc cette fameuse pyramide :"
 
-while i <= number
-	j.times {print " "}
-	i.times {print "#"}
-	puts "\n"
-	i = i + 1
-	j = j - 1
+	while i <= number
+		j.times {print " "}
+		i.times {print "#"}
+		puts "\n"
+		i = i + 1
+		j = j - 1
+	end
 end
