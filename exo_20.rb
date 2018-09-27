4871d5360c1bf4abfa4c0d450bd079ade768c2ca
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-09-28 00:30:18 +0200

    woops j'avais oublié la condition 1 a 25 étages max

--- exo_20.rb
+++ exo_20.rb
@@ -1,15 +1,20 @@
-puts "Salut, bienvenue dans le générateur de pyramide ! Combien d'étages veux-tu ?"
+puts "Salut, bienvenue dans le générateur de pyramide ! Combien d'étages veux-tu ? (attention le choix est limité entre 1 et 25 étages)"
 print ">  "
 
 number = gets.to_i
 
 i = 0
+if number < 1
+	puts "heu... je peux pas faire une pyramide avec ca"
+elsif number > 25
+	puts "On a dit pas + que 25 désolé"
+else
+	puts "Voici donc votre pyramide :"
+	while i <= number
+		i.times {print "#"}
+		puts "\n"
+		i = i + 1
+	end
 
-puts "Voici donc votre pyramide :"
-
-while i <= number
-	i.times {print "#"}
-	puts "\n"
-	i = i + 1
 end
 
